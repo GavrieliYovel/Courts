261cca6cb1e346431df71175b54aff261772f19e
itay45977 <itay45977@gmail.com>
2022-12-24 19:04:14 +0200

    Added edit + delete user

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -6,4 +6,7 @@ const usersRouter = new Router();
 usersRouter.get('/', usersDbController.getAllUsers);
 usersRouter.get('/:email', usersDbController.getUserByMail);
 usersRouter.post('/new', usersDbController.createUser );
+usersRouter.put('/edit/:email', usersDbController.editUser );
+usersRouter.delete('/:email', usersDbController.deleteUser );
+
 module.exports = { usersRouter };
