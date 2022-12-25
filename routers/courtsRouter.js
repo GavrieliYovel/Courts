8d2e35128dd712b87397bad90a09d8de1175f1e1
itay45977 <itay45977@gmail.com>
2022-12-25 13:45:45 +0200

    Added routes to two new functions

--- routers/courtsRouter.js
+++ routers/courtsRouter.js
@@ -1,9 +1,12 @@
 const { Router } = require('express');
 const { courtsDbController } = require('../controllers/courtsController')
+const {usersDbController} = require("../controllers/usersController");
 
 const courtsRouter = new Router();
 
 courtsRouter.get('/', courtsDbController.getAllUCourts);
 
 courtsRouter.post('/new', courtsDbController.createCourt);
+courtsRouter.put('/edit', courtsDbController.editCourt);
+courtsRouter.delete('/:name', courtsDbController.deleteCourt);
 module.exports = { courtsRouter };
