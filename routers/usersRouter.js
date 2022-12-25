3761e69cff7533311703074bba632ecb0f1be53c
itay45977 <itay45977@gmail.com>
2022-12-25 12:16:24 +0200

    Tried to add update instead of delete and create

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -6,7 +6,7 @@ const usersRouter = new Router();
 usersRouter.get('/', usersDbController.getAllUsers);
 usersRouter.get('/:email', usersDbController.getUserByMail);
 usersRouter.post('/new', usersDbController.createUser );
-usersRouter.put('/edit/:email', usersDbController.editUser );
+usersRouter.put('/edit', usersDbController.editUser );
 usersRouter.delete('/:email', usersDbController.deleteUser );
 
 module.exports = { usersRouter };
